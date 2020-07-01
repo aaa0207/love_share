cd8ba1afd6493bd4f29f45c57c4eb82823dd015a
wangxiaofan <2743033477@qq.com>
2020-07-01 16:39:23 +0800

    修改依赖

--- app/src/main/java/com/love/myapplication/MainActivity.java
+++ app/src/main/java/com/love/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.love.share.HttpUtils;
+import com.love.share.LoveShareClient;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoveShareClient.init(this, "123", new HttpUtils.StringCallback() {
+            @Override
+            public void onSuccess(String result) {
+                Log.e("xxx", result);
+            }
 
+            @Override
+            public void onFaileure(int code, Exception e) {
+                Log.e("xxx", code + e.toString());
+            }
+        });
 
     }
 }
--- love_share/src/main/java/com/love/share/LoveShareConstants.java
+++ love_share/src/main/java/com/love/share/LoveShareConstants.java
@@ -7,7 +7,7 @@ public class LoveShareConstants {
 
     private static final String TAG = "LoveShareConstants";
     public static int SDK_VERSION_CODE = 1;
-    public static String SDK_VERSION_NAME = "1.0.0";
+    public static String SDK_VERSION_NAME = "1.0.5";
     public static Context mApplicationContext;
     public static String Base_URL = "http://www.zhetengba.cn/server/test/copy/";
 }
